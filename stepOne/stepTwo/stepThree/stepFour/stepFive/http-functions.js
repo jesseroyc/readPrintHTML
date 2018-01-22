a611ff9a065657b31a6843015ddfa92ea4e7b10e
Jesse Roy-Cote <jesseroyc@gmail.com>
2018-01-22 15:53:42 -0700

    It is now a module

---
+++ stepOne/stepTwo/stepThree/stepFour/stepFive/http-functions.js
@@ -0,0 +1,25 @@
+module.exports = function getHTML (options, callback) {
+
+	var https = require('https');
+
+	var buffer = [];
+
+	https.get(options, function (response) {
+
+		//What does setting encoding mean???
+		response.setEncoding('utf8');
+
+		//Don't understand the .on function and the setEncoding function
+		response.on('data', function (data) {
+			console.log('Chunk Received. Length: ' + data.length + '\n');
+			buffer += data;
+				
+		});
+
+		response.on('end', function() {
+			console.log(buffer);
+	   		console.log('Response stream complete.')
+
+		});
+    });
+}
